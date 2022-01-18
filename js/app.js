5acb171f2c35dd516dcd976b0307da88e6b0d99a
= <theprofessordre@gmail.com>
2022-01-18 17:53:56 -0400

    Intro agregada

--- js/app.js
+++ js/app.js
@@ -1,44 +1,16 @@
-const intro = document.querySelector(".intro");
+const intro2 = document.querySelector(".intro-2");
+const btnContinue = document.querySelector(".intro-2 a");
 const audio = document.querySelector("#audio");
 const btnTime = document.querySelector(".time");
 const btnPlay = document.querySelector(".btn.play");
 const btnPause = document.querySelector(".btn.pause");
 
-intro.addEventListener("click", (e) => {
-  let body = document.querySelector("body");
-  let bullet = document.createElement("span");
-  bullet.classList.add("bullet");
-  let x = e.offsetX;
-  let y = e.offsetY;
+const content = document.getElementById("content");
+const contentAnime = document.querySelector(".content-anime");
+const textLines = document.querySelector(".text-lines");
 
-  bullet.style.left = `${x}px`;
-  bullet.style.top = `${y}px`;
-  body.appendChild(bullet);
-
-  let images = [
-    "url(./images/m1.png)",
-    "url(./images/m2.png)",
-    "url(./images/m3.png)",
-  ];
-
-  let bg = images[Math.floor(Math.random() * images.length)];
-
-  let blood = document.createElement("span");
-  blood.classList.add("blood");
-  blood.style.backgroundImage = bg;
-
-  blood.style.left = Math.random() * innerWidth + "px";
-  blood.style.top = Math.random() * innerHeight + "px";
-  body.appendChild(blood);
-
-  setTimeout(() => {
-    bullet.remove();
-    blood.remove();
-  }, 4000);
-
-  setTimeout(() => {
-    intro.classList.add("hideIntro");
-  }, 2000);
+btnContinue.addEventListener("click", (e) => {
+  intro2.style.display = "none";
 
   audio.play();
   btnTime.classList.add("play");
@@ -46,6 +18,10 @@ intro.addEventListener("click", (e) => {
 
   btnPlay.style.display = "none";
   btnPause.style.display = "block";
+
+  content.classList.add("active");
+  contentAnime.classList.add("active");
+  textLines.classList.add("active");
 });
 
 btnTime.addEventListener("click", () => {
